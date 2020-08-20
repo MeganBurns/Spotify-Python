f3a03464d7fc72b50a97edb86d6f3f0a6b895576
Megan Burns <m.burns272727@gmail.com>
2020-08-20 18:20:54 +1000

    updated playlist entries

--- spotifyPython.py
+++ spotifyPython.py
@@ -1,23 +1,23 @@
 import requests
 import json
-
 uris = []
 
+
 endpoint_url = "https://api.spotify.com/v1/recommendations?"
-access_token = "BQDhfioHLza9hiNRfpCXDGxEw2c7Jhkhhupkt2eMRXGQHtk6VH_FRA6NBpBVDH8ZDKKzvtGESYGr3i4-R0mcb8UMvoofbnLBFgGcRi3OVO_uV4tTC2-IbXVoUnoslqrKvDQNV4UqP9JhW8CRR7_oDbLgpwvV0FCytk5n-ArNd9Ui"
+access_token = "BQAvhhVNQnBVVOjMZea9yGgNNH18_VwUwMFefb-Sf7Fq5X3lyVpFo6al-GaEWqztCq9kQqyDsb6-R0cHeE3GppTVRnb0fr2b1yEFpBDoOSHN57ZIlwz7pyq45NeuScJHcZ1xpx_6tmvZ8QqNjZHNYsuXhSjVPvowG9m3wFzpPvmG"
 
 #Filters:
 limit = 10 #number of songs in playlist
 market = "AU" #country
-seed_genres = "pop" #genre
-target_danceability = 0.7 #on a scale from 0-1
-seed_artists = "06HL4z0CvFAxyc27GXpf02,0uq5PttqEjj3IH1bzwcrXF"
-
+#seed_genres = "indie-pop,pop" #genre
+#target_danceability = 0.7 #on a scale from 0-1
+#seed_artists = "06HL4z0CvFAxyc27GXpf02,3l0CmX0FuQjFxr8SK7Vqag,4QkSD9TRUnMtI8Fq1jXJJe"
+seed_tracks = "4PvbbMYL4fkToni5BLaYRb,2drtd6SptpMJ1KylMQ7mrE,43E0f1S0rOGCo6YYRYHjHP,3dNjUFt6EFU4Gq6Q5vfJqf,6Qjk6ZEltFhFqCIU2cp4Yd"
 
 
 #QUERY FOR SONGS
-query = f'{endpoint_url}limit={limit}&market={market}&seed_genres={seed_genres}&target_danceability={target_danceability}&seed_artists={seed_artists}'
-
+#query = f'{endpoint_url}limit={limit}&market={market}&seed_genres={seed_genres}&target_danceability={target_danceability}&seed_artists={seed_artists}'
+query = f'{endpoint_url}limit={limit}&market={market}&seed_tracks={seed_tracks}'
 
 response = requests.get(query,
                headers={"Content-Type":"application/json",
