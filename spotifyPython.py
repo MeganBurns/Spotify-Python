f1c839a279b4a04e90eb8c5aa7c86fb16ce9a5fc
Megan Burns <m.burns272727@gmail.com>
2020-08-27 16:54:48 +1000

    songs now add to spotify as a playlist

--- spotifyPython.py
+++ spotifyPython.py
@@ -4,20 +4,18 @@ uris = []
 
 
 endpoint_url = "https://api.spotify.com/v1/recommendations?"
-access_token = "BQAvhhVNQnBVVOjMZea9yGgNNH18_VwUwMFefb-Sf7Fq5X3lyVpFo6al-GaEWqztCq9kQqyDsb6-R0cHeE3GppTVRnb0fr2b1yEFpBDoOSHN57ZIlwz7pyq45NeuScJHcZ1xpx_6tmvZ8QqNjZHNYsuXhSjVPvowG9m3wFzpPvmG"
+access_token = "BQD1bPwc0X6dtooVuii-V278iyEE1h9Htv7vi-YMGewJIgjjtUn0K6L161CFBf_IUbmdN2BDbcxrVtEI-9QAAv16lJLyMhSISxBS-XPTA-iRqYSx6O4ecvsYSNv-RG8JqkZ84tAH-BIiGYKW7gfxYB4f_KPte7YWgqGh1NTIsCD8"
 
 #Filters:
-limit = 10 #number of songs in playlist
-market = "AU" #country
-#seed_genres = "indie-pop,pop" #genre
-#target_danceability = 0.7 #on a scale from 0-1
-#seed_artists = "06HL4z0CvFAxyc27GXpf02,3l0CmX0FuQjFxr8SK7Vqag,4QkSD9TRUnMtI8Fq1jXJJe"
-seed_tracks = "4PvbbMYL4fkToni5BLaYRb,2drtd6SptpMJ1KylMQ7mrE,43E0f1S0rOGCo6YYRYHjHP,3dNjUFt6EFU4Gq6Q5vfJqf,6Qjk6ZEltFhFqCIU2cp4Yd"
-
+limit = 15
+market = "AU"
+#seed_tracks = "4PvbbMYL4fkToni5BLaYRb,2drtd6SptpMJ1KylMQ7mrE,43E0f1S0rOGCo6YYRYHjHP,3dNjUFt6EFU4Gq6Q5vfJqf,6Qjk6ZEltFhFqCIU2cp4Yd"
+seed_tracks = "6VCeywT4JeawuZOUkQ1okx,1kqc6U8hVYZhY0gFGQclCz"
+seed_artists ="5DGJC3n9DS0Y9eY5ul9y0O,3l0CmX0FuQjFxr8SK7Vqag,6beUvFUlKliUYJdLOXNj9C"
 
 #QUERY FOR SONGS
 #query = f'{endpoint_url}limit={limit}&market={market}&seed_genres={seed_genres}&target_danceability={target_danceability}&seed_artists={seed_artists}'
-query = f'{endpoint_url}limit={limit}&market={market}&seed_tracks={seed_tracks}'
+query = f'{endpoint_url}limit={limit}&market={market}&seed_tracks={seed_tracks}&seed_artists={seed_artists}'
 
 response = requests.get(query,
                headers={"Content-Type":"application/json",
@@ -32,3 +30,26 @@ for i,j in enumerate(json_response['tracks']):
             print(f"{i+1}) \"{j['name']}\" by {j['artists'][0]['name']}")
 
 
+
+user_id = "megan272727"
+playlist_url = f"https://api.spotify.com/v1/users/{user_id}/playlists"
+
+request_body = json.dumps({
+          "name": "I coded this Playlist",
+          "description": "I'm going to create an environment so gay",
+          "public": False # let's keep it between us - for now
+        })
+
+response = requests.post(url = playlist_url, data = request_body, headers={"Content-Type":"application/json", "Authorization":"Bearer " + access_token})
+print(response.status_code)
+
+playlist_id = response.json()['id']
+
+
+fill_playlist = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+
+request_body = json.dumps({
+          "uris" : uris
+        })
+response = requests.post(url = fill_playlist, data = request_body, headers={"Content-Type":"application/json", "Authorization":f"Bearer {access_token}"})
+print(response.status_code)
